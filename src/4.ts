6625a9910e1bdab60c7d508a4008b08e79f36cc0
OsMax <www.osmax@gmail.com>
2023-10-29 01:48:18 +0300

    first try

--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,25 @@
+class Key {
+  someKey: Object;
+}
+
+class MyHouse {
+  constructor(private key: Key) {}
+  public openDoor(key: Key) {
+    console.log("Door is open.");
+  }
+  public comeIn(person: Person) {
+    console.log("Come into the house");
+  }
+}
+
+class Person {
+  constructor(private key: Key) {}
+  public getKey(): Key {
+    console.log("Take out the keys");
+    return this.key;
+  }
+}
+
 const key = new Key();
 
 const house = new MyHouse(key);
@@ -6,6 +28,5 @@ const person = new Person(key);
 house.openDoor(person.getKey());
 
 house.comeIn(person);
-
 
 export {};
